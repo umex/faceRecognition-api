b3fe7b90e06916d8e49b9938cf773c64a831a4cd
bomko <dejan.obrez@gmail.com>
2020-01-12 19:53:41 +0100

    fixed handle

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const db = knex({
 app.get('/',(req, res) => {res.send('it is working');})
 //dependency injection
 app.post('/signin',(req, res) => {signin.handleSignIn(req, res, db, bcrypt)})
-app.post('/register',  (req, res) => {register.handleSignIn(req, res, db, bcrypt)})
+app.post('/register',  (req, res) => {register.handleRegister(req, res, db, bcrypt)})
 app.get('/profile/:id',(req, res) => profile.handleProfile(req, res))
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res, db)})
